75814669c2fea3ed5a9705600045288a4b66fbe0
unknown <shahyug1122@gmail.com>
2026-08-27 12:55:21 +0530

    Prepare backend for deployment

--- backend/app.py
+++ backend/app.py
@@ -1284,7 +1284,6 @@ if __name__ == "__main__":
 
 
     app.run(
-        host="127.0.0.1",
-        port=5000,
-        debug=True
-    )
+    host="0.0.0.0",
+    port=int(os.getenv("PORT", 5000)),
+)
